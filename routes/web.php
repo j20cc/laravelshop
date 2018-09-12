8c286082b08d482ca51cde0eecdeb8f9ad7435d8
luke_44 <luke_44@foxmail.com>
2018-09-12 22:37:47 +0800

    product favor list

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 
@@ -34,6 +33,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
     // 结束
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
