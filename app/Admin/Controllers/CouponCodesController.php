49748d4e2d01adf7a2302a1bd44dc5311f826a15
lukedever <luke_44@foxmail.com>
2018-09-26 11:25:18 +0800

    管理优惠券

---
+++ app/Admin/Controllers/CouponCodesController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\CouponCode;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class CouponCodesController extends Controller
+{
+    use HasResourceActions;
+
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('优惠券列表');
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(CouponCode::class, function (Grid $grid) {
+            // 默认按创建时间倒序排序
+            $grid->model()->orderBy('created_at', 'desc');
+            $grid->id('ID')->sortable();
+            $grid->name('名称');
+            $grid->code('优惠码');
+            $grid->description('描述');
+            $grid->column('usage', '用量')->display(function () {
+                return "{$this->used} / {$this->total}";
+            });
+            $grid->enabled('是否启用')->display(function($value) {
+                return $value ? '是' : '否';
+            });
+            $grid->created_at('创建时间');
+
+            $grid->actions(function ($actions) {
+                $actions->disableView();
+            });
+        });
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(CouponCode::findOrFail($id));
+
+
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new CouponCode);
+
+
+
+        return $form;
+    }
+}
